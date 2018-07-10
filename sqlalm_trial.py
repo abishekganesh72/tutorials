50257b8112b0480e63c3ab5f690ea8e65e57abf0
Abishek Ganesh <abishekganesh72@gmail.com>
2018-07-10 14:19:14 -0500

    second commit
    Added desc,

--- sqlalm_trial.py
+++ sqlalm_trial.py
@@ -1,5 +1,7 @@
 # SQLAlchemy ORM
 
+# COde from : https://www.youtube.com/watch?v=51RpDZKShiw
+
 from sqlalchemy import create_engine
 
 engine = create_engine('sqlite:///:memory:', echo=True) # in Memory, echo =True
